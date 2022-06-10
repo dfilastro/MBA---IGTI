dcf0b79c635153a30905fd8cf450a04fcf6bf991
dfilastro <d.filastro@hotmail.com>
2022-06-10 16:50:39 -0300

    first tests created

--- Back-end/4 - Topicos Especiais Back End/1 - Aulas/src/calcula-valor.js
+++ Back-end/4 - Topicos Especiais Back End/1 - Aulas/src/calcula-valor.js
@@ -1,6 +1,33 @@
-function calcValue(cap, tx, per) {
-  let amount = cap * Math.pow(1 + tx, per - 1);
-  return amount;
+function roundedNumber(num) {
+  const roundedNum = Number(num.toFixed(2));
+  return roundedNum;
 }
 
-export { calcValue };
+// function calcValue(cap, tx, per) {
+//   let amount = Number((cap * Math.pow(1 + tx, per - 1)).toFixed(2));
+//   return amount;
+// }
+
+function calcValue(cap, tx, per) {
+  const amount = cap * Math.pow(1 + tx, per - 1);
+  return roundedNumber(amount);
+}
+
+function calcPayments(val, num) {
+  const result = [];
+
+  const paymentValue = roundedNumber(val / num);
+  const leftovers = val - paymentValue * num;
+  const firstPayment = paymentValue + leftovers;
+  result.push(firstPayment);
+
+  for (let i = 1; i < num; i++) {
+    result.push(paymentValue);
+  }
+
+  return result;
+}
+
+console.log(calcPayments(101.994, 3));
+
+export { calcValue, roundedNumber, calcPayments };
--- Back-end/4 - Topicos Especiais Back End/1 - Aulas/test/calcula-valor.test.js
+++ Back-end/4 - Topicos Especiais Back End/1 - Aulas/test/calcula-valor.test.js
@@ -1,9 +1,80 @@
-import { calcValue } from '../src/calcula-valor.js';
+import { calcValue, roundedNumber, calcPayments } from '../src/calcula-valor.js';
 
-test('If it was payed in one time period, the value doesn`t receives interest', () => {
-  // Operação
-  const amount = calcValue(100, 0.0175, 1);
+describe('Calc future value', () => {
+  test('If it was payed in one time period, the value doesn`t receives interest', () => {
+    // Operação
+    const amount = calcValue(100, 0.0175, 1);
 
-  // Resultado/Comportamento esperado
-  expect(amount).toBe(100);
+    // Resultado/Comportamento esperado
+    expect(amount).toBe(100);
+  });
+
+  test('Calc value with 4 payments', () => {
+    const amount = calcValue(500, 0.025, 4);
+
+    expect(amount).toBe(538.45);
+  });
+});
+
+describe('Rounded number function', () => {
+  test('Round a number with 2 decimal places', () => {
+    const number = roundedNumber(538.4453124999998);
+    expect(number).toBe(538.45);
+  });
+
+  // test('Another test with rounded number', () => {
+  //   const number = roundedNumber(1.005);
+  //   expect(number).toBe(1.01);
+  // });
+});
+
+describe('Payments array', () => {
+  test('Checks the number of payments', () => {
+    // Premissas
+    const numPayments = 6;
+
+    // Operação
+    const payments = calcPayments(200, numPayments);
+
+    // Resultado esperado
+    expect(payments.length).toBe(numPayments);
+  });
+
+  test('In one time, the value must be the same, with no interest', () => {
+    // Premissas
+    const numPayments = 1;
+
+    // Operação
+    const payments = calcPayments(200, numPayments);
+
+    // Resultado esperado
+    expect(payments.length).toBe(numPayments);
+    expect(payments[0]).toBe(200);
+  });
+
+  test('Checks if the first payment receives the leftovers', () => {
+    // Premissas
+    const numPayments = 3;
+    const value = 100;
+
+    // Operação
+    const payments = calcPayments(value, numPayments);
+
+    // Resultado esperado
+    expect(payments.length).toBe(numPayments);
+    expect(payments[0] + payments[1] + payments[2]).toBe(value);
+  });
+
+  test('A value not integer', () => {
+    // Premissas
+    const numPayments = 3;
+    const value = 101.994;
+
+    // Operação
+    const payments = calcPayments(value, numPayments);
+
+    // Resultado esperado
+    expect(payments.length).toBe(numPayments);
+    expect(payments[0] + payments[1] + payments[2]).toBe(value);
+  });
 });
